c1e0414bae1d4035e85c5fa24d5425f8b0310310
stefanperkovic <stefan.perkovic@menloschool.org>
2025-02-09 20:21:17 -0800

    Solved using 1D array

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  */
 
 public class MakingChange {
-    // Memoization solution to the problem set
+//    // Memoization solution to the problem set
 //    public static long countWays(int target, int[] coins) {
 //        long[][] arr = new long[target + 1][coins.length];
 //        for (int i = 0; i <= target; i++) {
@@ -38,23 +38,39 @@ public class MakingChange {
 //        return count;
 //    }
 //
-    // Tabulation Solution
+//    // Tabulation Solution
+//    public static long countWays(int target, int[] coins) {
+//        long[][] dynamic = new long[target + 1][coins.length + 1];
+//        // Base case as there is one way to make 0 amount with any number of coins
+//        for (int j = 0; j <= coins.length; j++) {
+//            dynamic[0][j] = 1;
+//        }
+//        // Building the dynamic table
+//        for(int i = 1; i <= target; i++){
+//            for(int j = 1; j <= coins.length; j++){
+//                // Number of ways without considering the current coin
+//                dynamic[i][j] = dynamic[i][j - 1];
+//                if (i - coins[j - 1] >= 0){
+//                    dynamic[i][j] += dynamic[i - coins[j - 1]][j];
+//                }
+//            }
+//        }
+//        return dynamic[target][coins.length];
+//    }
+//}
     public static long countWays(int target, int[] coins) {
-        long[][] dynamic = new long[target + 1][coins.length + 1];
-        // Base case as there is one way to make 0 amount with any number of coins
-        for (int j = 0; j <= coins.length; j++) {
-            dynamic[0][j] = 1;
-        }
-        // Building the dynamic table
-        for(int i = 1; i <= target; i++){
-            for(int j = 1; j <= coins.length; j++){
-                // Number of ways without considering the current coin
-                dynamic[i][j] = dynamic[i][j - 1];
-                if (i - coins[j - 1] >= 0){
-                    dynamic[i][j] += dynamic[i - coins[j - 1]][j];
-                }
+        // Create a 1D array to store the number of ways to make each amount
+        long[] dynamic = new long[target + 1];
+        dynamic[0] = 1;
+        // Iterate through each coin
+        for (int coin : coins) {
+            // For each coin update the dynamic array
+            for (int i = coin; i <= target; i++) {
+                // Number of ways to make 'i' is increased by number of ways to make 'i - coin'
+                dynamic[i] += dynamic[i - coin];
             }
         }
-        return dynamic[target][coins.length];
+        return dynamic[target];
+
     }
 }
